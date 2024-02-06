bb1c6f7cecf74624433466b60a70afc743bbdc83
Emilly <emilly.silva@meta.com.br>
2024-02-06 11:21:18 -0300

    jogo da forca

--- output/forca.c
+++ output/forca.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define TAMANHO_PALAVRA 20
+
 //Declaração Funções -----------------------------------------------------------------------------------------------------------------------------------------------------------------
 //--> Para não precisar se preocupar com a ordem em que chamamos as funções
 void header();
@@ -12,10 +14,12 @@ void escolhepalavra();
 int acertou();
 int enforcou ();
 int jachutou (char letra);
+void adicionapalavra();
+int chuteserrados();
 
 //Variáveis Globais ------------------------------------------------------------------------------------------------------------------------------------------------------------------
 size_t tamanho;
-char palavrasecreta[20];
+char palavrasecreta[TAMANHO_PALAVRA];
 char chutes[26];
 int chutesdados = 0;
 
@@ -50,6 +54,41 @@ int jachutou(char letra) {
     return achou;
 }
 
+// --> Adiciona uma nova pavra para a lista da forca
+void adicionapalavra(){
+    char quer;
+
+    printf("Voce deseja adicionar uma nova palavra no jogo? (S/N):\n");
+    scanf(" %c", &quer);
+
+        if (quer == 'S'){
+            char novapalavra[TAMANHO_PALAVRA];
+
+            printf("Qual a nova palavra?\n");
+            scanf("%s", novapalavra);
+
+            FILE* f;
+            f = fopen("palavras.txt", "r+"); //Lê o arquivo e permite escrever nele
+            
+            if(f == 0){
+                printf("Desculpe, banco de dados nao disponivel!\n\n");
+                exit(1);
+            }
+
+            int qtd = 0;
+            fscanf(f,"%d", &qtd); //Lê a quantidade palavras que o arquivo tem (que está na primeira linha)
+            qtd++; //Soma + 1 na quantidade - pois foi acrescentada uma palavra
+
+            fseek(f, 0, SEEK_SET); //Vai voltar para a primeira linha, para setar o valor de quantidade de palavras
+            fprintf(f,"%d", qtd);
+
+            fseek(f, 0, SEEK_END); //Posiciona novamente a seta no final do arquivo
+            fprintf(f, "\n%s", novapalavra); //Escreve a nova palavra
+
+            fclose(f);
+        }
+}
+
 //--> Palavra do jogo
 void escolhepalavra(){
 
@@ -70,7 +109,7 @@ void escolhepalavra(){
     for (int i=0; i <= randomico;i++){
         fscanf(f, "%s", palavrasecreta);
     }
-    
+
     tamanho = strlen(palavrasecreta);
 
     fclose(f);//é necessário fechar o arquivo
@@ -78,6 +117,22 @@ void escolhepalavra(){
 
 //--> Desenha Forca e Letras acertadas
 void desenhaforca() {
+
+    int erros = chuteserrados();
+
+//if ternário -> (condicao ? valor verdadeiro : valor falso)
+    printf("  _______       \n");
+    printf(" |/      |      \n");
+    printf(" |      %c%c%c  \n", (erros>=1?'(':' '), 
+        (erros>=1?'_':' '), (erros>=1?')':' '));
+    printf(" |      %c%c%c  \n", (erros>=3?'\\':' '), 
+        (erros>=2?'|':' '), (erros>=3?'/': ' '));
+    printf(" |       %c     \n", (erros>=2?'|':' '));
+    printf(" |      %c %c   \n", (erros>=4?'/':' '), 
+        (erros>=4?'\\':' '));
+    printf(" |              \n");
+    printf("_|___           \n");
+    printf("\n\n");
 
     printf("Voce ja deu %d chutes\n", chutesdados);
 
@@ -92,6 +147,7 @@ void desenhaforca() {
     printf("\n");
 }
 
+// --> Acertos da palavra
 int acertou(){
     for (int i = 0; i < (int)tamanho; i++){
         if(!jachutou(palavrasecreta[i])){
@@ -101,8 +157,8 @@ int acertou(){
     return 1;
 }
 
-//--> Identifica se o número de chutes foi atingido
-int enforcou(){
+// --> Identifica os chutes errados
+int chuteserrados(){
     int erros = 0;
     for (int i = 0; i < chutesdados; i++){
         int existe = 0;
@@ -115,7 +171,14 @@ int enforcou(){
         }
         if(!existe) erros++;
     }
-    return erros >= 5;
+
+    return erros;
+}
+
+//--> Identifica se o número de chutes foi atingido
+int enforcou(){
+
+    return chuteserrados() >= 5;
 }
 
 //Main -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -129,4 +192,12 @@ int main() {
         chuta();
   
     }while(!acertou() && !enforcou());
+
+    if(acertou()){
+        printf("PARABENS PELA VITORIA! VOCE GANHOU!!!!!!! :D\n");
+    }else{
+        printf("VOCE PERDEU! TENTE NA PROXIMA *_*\n");
+    }
+
+    adicionapalavra();
 }
